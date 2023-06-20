d1910e0014af1723cec674cda2f7603386f660e7
samportan <samijtucker@gmail.com>
2023-06-20 12:18:10 -0600

    bugs fixed

--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/recipes/RecipesFragment.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/recipes/RecipesFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.rockettsttudio.eatsease.databinding.FragmentRecipesBinding
@@ -26,13 +25,9 @@ class RecipesFragment : Fragment() {
             ViewModelProvider(this).get(RecipesViewModel::class.java)
 
         _binding = FragmentRecipesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textRecipes
-        recipesViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = "Recipes"
-        }
-        return root
+
+        return binding.root
     }
 
     override fun onDestroyView() {
--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/saved/SavedFragment.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/saved/SavedFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.rockettsttudio.eatsease.databinding.FragmentSavedBinding
@@ -26,13 +25,8 @@ class SavedFragment : Fragment() {
             ViewModelProvider(this).get(SavedViewModel::class.java)
 
         _binding = FragmentSavedBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textSaved
-        savedViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = "Saved"
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
