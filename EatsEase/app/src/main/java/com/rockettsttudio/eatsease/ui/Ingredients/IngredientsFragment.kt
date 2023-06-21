a2d1378174b1eb2db00caf7e7bc20f332da4d818
Cesar Roque <00015319@uca.edu.sv>
2023-06-21 16:54:12 -0600

    Add::Class_Ingredients

---
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/Ingredients/IngredientsFragment.kt
@@ -0,0 +1,21 @@
+package com.rockettsttudio.eatsease.ui.Ingredients
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.rockettsttudio.eatsease.R
+
+class IngredientsFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_ingredientes, container, false)
+    }
+
+}
