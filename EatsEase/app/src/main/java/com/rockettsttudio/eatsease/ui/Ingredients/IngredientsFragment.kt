a51919fec6bf414d7008ce6c1de11a7d6164e556
Cesar Roque <00015319@uca.edu.sv>
2023-06-21 17:31:54 -0600

    Update_kt

--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/Ingredients/IngredientsFragment.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/Ingredients/IngredientsFragment.kt
@@ -4,18 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.rockettsttudio.eatsease.R
+import com.rockettsttudio.eatsease.databinding.FragmentIngredientesBinding
 
-class IngredientsFragment : Fragment(){
+class IngredientsFragment : Fragment() {
 
-    override fun onCreateView(
+    private lateinit var checkboxIngredients: CheckBox
+
+    private var _binding: FragmentIngredientesBinding? = null
+    private val binding get() = _binding!!
+
+   override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_ingredientes, container, false)
-    }
 
+   }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
