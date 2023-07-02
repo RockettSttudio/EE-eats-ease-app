d895fc35bee09a8f5ec3c63cd3ee75db3cffccf6
XavierMoran <00106121@uca.edu.sv>
2023-07-01 21:59:50 -0600

    add: add_calendar

--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/recipes/RecipeDetailsFragment.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/recipes/RecipeDetailsFragment.kt
@@ -3,7 +3,6 @@ package com.rockettsttudio.eatsease.ui.recipes
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +13,8 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.rockettsttudio.eatsease.databinding.FragmentRecipeDetailsBinding
+import android.content.Intent
+import android.provider.CalendarContract
 
 class RecipeDetailsFragment : Fragment() {
 
@@ -35,6 +36,13 @@ class RecipeDetailsFragment : Fragment() {
 
         binding.backFlechaReceta.setOnClickListener {
             findNavController().navigateUp()
+        }
+
+        binding.addCalendar.setOnClickListener {
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.Events.TITLE, binding.titleDetailView.text.toString())
+            startActivity(intent)
         }
 
         binding.titleDetailView.text = arguments?.getString("title")
