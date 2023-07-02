549857e7483bf518e8739c135d071c3480879d82
Cesar Roque <00015319@uca.edu.sv>
2023-07-02 14:45:33 -0600

    UPDATE::fragment

--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/recipes/RecipeSaved.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/recipes/RecipeSaved.kt
@@ -10,9 +10,9 @@ import com.rockettsttudio.eatsease.databinding.FragmentIngredientsBinding
 import com.rockettsttudio.eatsease.databinding.FragmentSavedBinding
 
 class RecipeSaved : Fragment()  {
+
     private var _binding: FragmentSavedBinding? =null
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
