00d0dfd1bd5f69489a2923b51e139f44397a8235
danielquesada <00147619@uca.edu.sv>
2023-07-02 23:35:08 -0600

    Saved page now works

--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/recipes/RecipeDetailsFragment.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/recipes/RecipeDetailsFragment.kt
@@ -17,17 +17,35 @@ import android.content.Intent
 import android.provider.CalendarContract
 import android.widget.Button
 import android.widget.CheckBox
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 
 class RecipeDetailsFragment : Fragment() {
 
     private lateinit var binding: FragmentRecipeDetailsBinding
     private var onBackPressedCallback: OnBackPressedCallback? = null
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var currentUser: FirebaseUser
+    private lateinit var favoritesRef: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        // Set up Firebase Authentication
+        auth = FirebaseAuth.getInstance()
+        currentUser = auth.currentUser!!
+
+        val database = FirebaseDatabase.getInstance()
+        favoritesRef = database.reference.child("users").child(currentUser.uid).child("favorites")
 
         binding = FragmentRecipeDetailsBinding.inflate(inflater, container, false)
 
@@ -40,14 +58,31 @@ class RecipeDetailsFragment : Fragment() {
         binding.backFlechaReceta.setOnClickListener {
             findNavController().navigateUp()
         }
-        binding.favoriteButton.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                // Checkbox is checked, perform desired action
-            } else {
-                // Checkbox is unchecked, perform desired action
-            }
-        }
 
+        val recipeTitle = arguments?.getString("title") // Titulo
+        binding.titleDetailView.text = arguments?.getString("title")
+        val imgUrl = arguments?.getString("image") // IMAGEN
+        val sourceURL = arguments?.getString("sourceURL") // LINK
+        Glide.with(requireContext()).load(imgUrl).into(binding.imageDetailsView)
+        val recipeSummary = arguments?.getString("summary") // descripcion
+        binding.descDetailsView.text = arguments?.getString("summary")
+        val recipeIngredients = arguments?.getString("ingredients")
+        binding.ingredientsDetailsView.text = arguments?.getString("ingredients")
+        val recipeInstr = arguments?.getString("instructions") // instrucciones
+        binding.instructionsDetailsView.text = arguments?.getString("instructions")
+
+        // Check if the recipe is already in the user's favorites
+        favoritesRef.orderByChild("recipeTitle").equalTo(recipeTitle)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val isFavorite = snapshot.exists()
+                    binding.favoriteButton.isChecked = isFavorite
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Handle database error
+                }
+            })
 
         binding.addCalendar.setOnClickListener {
             val intent = Intent(Intent.ACTION_INSERT)
@@ -55,18 +90,6 @@ class RecipeDetailsFragment : Fragment() {
                 .putExtra(CalendarContract.Events.TITLE, binding.titleDetailView.text.toString())
             startActivity(intent)
         }
-
-        val recipeTitle = arguments?.getString("title") //Titulo
-        binding.titleDetailView.text = arguments?.getString("title")
-        val imgUrl = arguments?.getString("image") //IMAGEN
-        val sourceURL = arguments?.getString("sourceURL") //LINK
-        Glide.with(requireContext()).load(imgUrl).into(binding.imageDetailsView)
-        val recipeSummary = arguments?.getString("summary") //descripcion
-        binding.descDetailsView.text = arguments?.getString("summary")
-        val recipeIngredients = arguments?.getString("ingredients")
-        binding.ingredientsDetailsView.text = arguments?.getString("ingredients")
-        val recipeInstr =  arguments?.getString("instructions") //instruciones
-        binding.instructionsDetailsView.text = arguments?.getString("instructions")
 
         onBackPressedCallback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
@@ -85,12 +108,148 @@ class RecipeDetailsFragment : Fragment() {
             shareIntent.putExtra(Intent.EXTRA_TEXT, shareText)
             startActivity(Intent.createChooser(shareIntent, "Compartir receta"))
         }
+
+        binding.favoriteButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Checkbox is checked, add to favorites
+                if (recipeTitle != null && sourceURL != null && recipeSummary != null &&
+                    recipeIngredients != null && recipeInstr != null && imgUrl != null
+                ) {
+                    addToFavorite(
+                        requireContext(),
+                        recipeTitle,
+                        sourceURL,
+                        recipeSummary,
+                        recipeIngredients,
+                        recipeInstr,
+                        imgUrl
+                    )
+                } else {
+                    // Handle the case where any of the required arguments is null
+                    Toast.makeText(context, "Missing recipe details", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                // Checkbox is unchecked, remove from favorites
+                if (recipeTitle != null) {
+                    removeFromFavorites(requireContext(), recipeTitle)
+                } else {
+                    // Handle the case where favoriteId is null
+                    Toast.makeText(context, "Missing favorite ID", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
+
+    private fun addToFavorite(
+        context: Context,
+        recipeTitle: String,
+        sourceURL: String,
+        recipeSummary: String,
+        recipeIngredients: String,
+        recipeInstr: String,
+        imgUrl: String
+    ) {
+        // Check if the user is authenticated
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            // Get a reference to the Firebase Realtime Database
+            val database = FirebaseDatabase.getInstance()
+            val favoritesRef = database.reference
+                .child("users")
+                .child(currentUser.uid)
+                .child("favorites")
+
+            // Check if the favorite already exists
+            favoritesRef.orderByChild("sourceURL")
+                .equalTo(sourceURL)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            // Favorite already exists, show a message or handle it accordingly
+                            Toast.makeText(context, "Already added to favorites", Toast.LENGTH_SHORT).show()
+                        } else {
+                            // Create a new entry in the "favorites" node
+                            val favoriteEntry = favoritesRef.push()
+                            val favoriteId = favoriteEntry.key
+
+                            // Create a map of the data you want to store
+                            val favoriteData = hashMapOf(
+                                "recipeTitle" to recipeTitle,
+                                "sourceURL" to sourceURL,
+                                "recipeSummary" to recipeSummary,
+                                "recipeIngredients" to recipeIngredients,
+                                "recipeInstructions" to recipeInstr,
+                                "imgUrl" to imgUrl
+                            )
+
+                            // Set the data in the database under the generated favoriteId
+                            favoriteEntry.setValue(favoriteData)
+                                .addOnSuccessListener {
+                                    // Data successfully added to the database
+                                    Toast.makeText(context, "Added to favorites", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener {
+                                    // Error occurred while adding data to the database
+                                    Toast.makeText(context, "Failed to add to favorites", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // Handle the error
+                        Toast.makeText(context, "Failed to check favorite status", Toast.LENGTH_SHORT).show()
+                    }
+                })
+        }
+    }
+
+
+
+    private fun removeFromFavorites(context: Context, recipeTitle: String) {
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            val database = FirebaseDatabase.getInstance()
+            val favoritesRef = database.reference
+                .child("users")
+                .child(currentUser.uid)
+                .child("favorites")
+
+            favoritesRef.orderByChild("recipeTitle")
+                .equalTo(recipeTitle)
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            // Recipe found, remove it from favorites
+                            val favoriteSnapshot = dataSnapshot.children.first()
+                            favoriteSnapshot.ref.removeValue()
+                                .addOnSuccessListener {
+                                    // Successfully removed from favorites
+                                    Toast.makeText(context, "Removed from favorites", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener {
+                                    // Error occurred while removing from favorites
+                                    Toast.makeText(context, "Failed to remove from favorites", Toast.LENGTH_SHORT).show()
+                                }
+                        } else {
+                            // Recipe not found in favorites
+                            Toast.makeText(context, "Recipe not found in favorites", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        // Handle the error
+                        Toast.makeText(context, "Failed to remove from favorites", Toast.LENGTH_SHORT).show()
+                    }
+                })
+        }
+    }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         onBackPressedCallback?.remove()
         onBackPressedCallback = null
     }
-
 }
--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/saved/SavedFragment.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/saved/SavedFragment.kt
@@ -4,33 +4,103 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.rockettsttudio.eatsease.R
+import com.rockettsttudio.eatsease.data.models.Recipe
+import com.rockettsttudio.eatsease.data.models.SavedRecipes
 import com.rockettsttudio.eatsease.databinding.FragmentSavedBinding
 
-class SavedFragment : Fragment() {
+class SavedFragment : Fragment(), SavedAdapter.OnItemClickListener {
 
     private var _binding: FragmentSavedBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var savedAdapter: SavedAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val savedViewModel =
-            ViewModelProvider(this).get(SavedViewModel::class.java)
-
         _binding = FragmentSavedBinding.inflate(inflater, container, false)
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize RecyclerView and adapter
+        savedAdapter = SavedAdapter(this)
+        binding.recyclerviewSaved.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerviewSaved.adapter = savedAdapter
+
+        // Add your logic to fetch the list of saved recipes from the database
+        // and pass it to the adapter using setRecipes() method
+        val savedRecipes = getSavedRecipesFromDatabase()
+        savedAdapter.setRecipes(savedRecipes)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onItemClick(recipe: SavedRecipes) {
+        // Handle item click event
+        val bundle = Bundle().apply {
+            putString("title", recipe.recipeTitle)
+            putString("image", recipe.imgUrl)
+            val summary = HtmlCompat.fromHtml(recipe.recipeSummary, HtmlCompat.FROM_HTML_MODE_LEGACY).toString()
+            putString("summary", summary)
+            putString("sourceURL", recipe.sourceURL)
+            putString("ingredients", recipe.recipeIngredients)
+            putString("instructions", recipe.recipeInstructions)
+        }
+
+        // Navigate to the RecipeDetailFragment and pass the bundle
+        val navController = findNavController()
+        navController.navigate(R.id.action_navigation_saved_to_recipeDetailsFragment, bundle)
+    }
+
+    private fun getSavedRecipesFromDatabase(): List<SavedRecipes> {
+        val database = FirebaseDatabase.getInstance()
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        val recipesRef = userId?.let { database.getReference("users").child(it).child("favorites") }
+        val savedRecipes = mutableListOf<SavedRecipes>()
+
+        recipesRef?.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                savedRecipes.clear()
+                for (recipeSnapshot in dataSnapshot.children) {
+                    val recipe = recipeSnapshot.getValue(SavedRecipes::class.java)
+                    recipe?.let { savedRecipes.add(it) }
+                }
+                savedAdapter.setRecipes(savedRecipes)
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle database error
+            }
+        })
+
+        return savedRecipes
+    }
 }
+
+
+
+
+
+
