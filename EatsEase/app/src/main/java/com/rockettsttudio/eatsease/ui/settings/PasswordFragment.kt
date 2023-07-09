6604fac0a1744d8d29510bdb706450da9bdb23cd
danielquesada <00147619@uca.edu.sv>
2023-07-09 13:04:47 -0600

    password change function

--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/settings/PasswordFragment.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/settings/PasswordFragment.kt
@@ -1,32 +1,25 @@
 package com.rockettsttudio.eatsease.ui.settings
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.rockettsttudio.eatsease.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PasswordFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PasswordFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,27 +27,61 @@ class PasswordFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_paswword, container, false)
+        val view = inflater.inflate(R.layout.fragment_paswword, container, false)
+
+        val currentPasswordEditText = view.findViewById<EditText>(R.id.etCurrentPassword)
+        val newPasswordEditText = view.findViewById<EditText>(R.id.etNewPassword2)
+        val updatePasswordButton = view.findViewById<Button>(R.id.updatePasswordButton)
+
+        updatePasswordButton.setOnClickListener {
+            val currentPassword = currentPasswordEditText.text.toString()
+            val newPassword = newPasswordEditText.text.toString()
+
+            // Get the current user from Firebase Authentication
+            val user = FirebaseAuth.getInstance().currentUser
+
+            // Check if the user is signed in with Google
+            val isGoogleUser = user?.providerData?.any { it.providerId == GoogleAuthProvider.PROVIDER_ID } ?: false
+
+            if (isGoogleUser) {
+                // User is logged in with Google, provide instructions to change password with Google
+                showToast("Since you're logged in with Google, please update your password directly through Google account settings.")
+                openGoogleAccountSettings()
+            } else {
+                // Reauthenticate the user with their current password
+                val credential = EmailAuthProvider.getCredential(user!!.email!!, currentPassword)
+                user.reauthenticate(credential)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            // User re-authenticated successfully, now change the password
+                            user.updatePassword(newPassword)
+                                .addOnCompleteListener { passwordUpdateTask ->
+                                    if (passwordUpdateTask.isSuccessful) {
+                                        // Password updated successfully
+                                        showToast("Password updated successfully")
+                                    } else {
+                                        // Password update failed
+                                        showToast("Failed to update password")
+                                    }
+                                }
+                        } else {
+                            // User re-authentication failed
+                            showToast("Failed to re-authenticate")
+                        }
+                    }
+            }
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PaswwordFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PasswordFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun openGoogleAccountSettings() {
+        val googleAccountUri = Uri.parse("https://myaccount.google.com/security")
+        val openSettingsIntent = Intent(Intent.ACTION_VIEW, googleAccountUri)
+        startActivity(openSettingsIntent)
     }
 }
