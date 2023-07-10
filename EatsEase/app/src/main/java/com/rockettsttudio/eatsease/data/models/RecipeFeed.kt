6b21224c55f1af9a08938aa134ad3d4e7495e67c
samportan <samijtucker@gmail.com>
2023-07-09 18:13:58 -0600

    home feed added

--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/data/models/RecipeFeed.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/data/models/RecipeFeed.kt
@@ -1,3 +1,13 @@
 package com.rockettsttudio.eatsease.data.models
 
-data class RecipeFeed()
+import com.google.gson.annotations.SerializedName
+
+data class RecipeFeed(
+    @SerializedName("id") val id: Int,
+    @SerializedName("user_id") val userId: String,
+    @SerializedName("title") val title: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("ingredients") val ingredients: String,
+    @SerializedName("instructions") val instructions: String,
+    @SerializedName("created_at") val createdAt: String
+)
--- EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/home/FeedAdapter.kt
+++ EatsEase/app/src/main/java/com/rockettsttudio/eatsease/ui/home/FeedAdapter.kt
@@ -1,4 +1,66 @@
 package com.rockettsttudio.eatsease.ui.home
 
-class FeedAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.rockettsttudio.eatsease.R
+import com.rockettsttudio.eatsease.data.models.RecipeFeed
+
+class FeedAdapter(
+    private var recipes: List<RecipeFeed>,
+    private val itemClickListener: HomeFragment
+) : RecyclerView.Adapter<FeedAdapter.RecipeViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecipeViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.home_item, parent, false)
+        return RecipeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        val recipe = recipes[position]
+    }
+
+    override fun getItemCount(): Int {
+        return recipes.size
+    }
+
+    fun setRecipes(recipes: List<RecipeFeed>) {
+        this.recipes = recipes
+        notifyDataSetChanged()
+    }
+
+    inner class RecipeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView),
+        View.OnClickListener {
+        private val recipeImageView: ImageView = itemView.findViewById(R.id.recipe_image_view_home)
+        private val titleTextView: TextView = itemView.findViewById(R.id.recipe_title_view_home)
+        private val descTextView: TextView = itemView.findViewById(R.id.recipe_desc_view_home)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(recipe: RecipeFeed) {
+            titleTextView.text = recipe.title
+            descTextView.text = recipe.description
+            Glide.with(itemView.context).load(R.drawable.svg_layer4).into(recipeImageView)
+        }
+
+        override fun onClick(v: View) {
+            val position = bindingAdapterPosition
+            if (position != RecyclerView.NO_POSITION) {
+                val clickedRecipe = recipes[position]
+                itemClickListener.onItemClick(clickedRecipe)
+            }
+        }
+
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(recipe: RecipeFeed)
+    }
+
 }
